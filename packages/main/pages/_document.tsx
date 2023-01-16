2e6b3f2bd5b75629328af61b5da3ce98a0b6804a
Ivan Montiel <idmontie@gmail.com>
2023-01-15 17:15:17 -0700

    fix: remove light to dark theme flicker

--- packages/main/pages/_document.tsx
+++ packages/main/pages/_document.tsx
@@ -9,6 +9,26 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Open+Sans:wght@300;400;700&display=swap"
                         rel="stylesheet"
                     />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                        try {
+                            const storage = window && window.localStorage;
+                            if (
+                                storage.getItem("color-theme") === "dark" ||
+                                (!("color-theme" in storage) &&
+                                    window.matchMedia("(prefers-color-scheme: dark)").matches)
+                            ) {
+                                document.documentElement.classList.add("dark");
+                            } else {
+                                document.documentElement.classList.remove("dark");
+                            }
+                        } catch (e) {
+                            console.error(e);
+                        }
+                        `,
+                        }}
+                    ></script>
                 </Head>
                 <body className="leading-base bg-white text-lg antialiased dark:bg-gray-900 dark:text-white">
                     <Main />
