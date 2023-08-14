84a941a19cb191922030831dc4023dce5ab2e9e2
idmontie <idmontie@users.noreply.github.com>
2023-08-14 17:22:25 +0000

    deploy: 7b6d1e737ca4c28f0942aba1022e31d71f78dd4d

--- _next/static/u7f9GnEl5eUwHpo2DYBNP/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblog","\u002Fblog\u002F[page]","\u002Fblog\u002Fpost\u002F[slug]","\u002Fportfolio","\u002Fprojects\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
