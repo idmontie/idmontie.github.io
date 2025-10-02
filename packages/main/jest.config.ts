0cdf79de86315f17714dc253283f9bb3c127e5a6
Ivan Montiel <idmontie@gmail.com>
2025-10-02 09:45:00 -0700

    test: introduce coverage command

--- packages/main/jest.config.ts
+++ packages/main/jest.config.ts
@@ -8,4 +8,32 @@ export default {
     moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
     coverageDirectory: "../../coverage/packages/main",
     setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+
+    // Include un-coveraged files in the coverage report
+    collectCoverageFrom: [
+        "**/*.{ts,tsx,js,jsx}",
+        "!**/*.spec.{ts,tsx,js,jsx}",
+        "!index.d.ts",
+        "!jest.config.ts",
+        "!next-env.d.ts",
+        "!next.config.js",
+        "!postcss.config.js",
+        "!tailwind.config.js",
+        "!**/.next/**",
+        "!**/node_modules/**",
+        "!**/dist/**",
+        "!**/build/**",
+        "!**/coverage/**",
+        "!**/public/**",
+        "!**/styles/**",
+    ],
+    coverageReporters: ["json", "lcov", "text", "clover"],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
 };
