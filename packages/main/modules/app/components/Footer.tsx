398a96b908b81565a845c926fffa81b358f4e338
idmontie <idmontie@gmail.com>
2026-09-17 10:12:05 -0700

    feat: add bluesky link

--- packages/main/modules/app/components/Footer.tsx
+++ packages/main/modules/app/components/Footer.tsx
@@ -43,6 +43,19 @@ function MediumIcon() {
     );
 }
 
+function BlueskyIcon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mx-auto h-full w-4"
+            viewBox="0 0 600 530"
+            fill="currentColor"
+        >
+            <path d="m135.72 44.03c66.496 49.921 138.02 151.14 164.28 205.46 26.262-54.316 97.782-155.54 164.28-205.46 47.98-36.021 125.72-63.892 125.72 24.795 0 17.712-10.155 148.79-16.111 170.07-20.703 73.984-96.144 92.854-163.25 81.433 117.3 19.964 147.14 86.092 82.697 152.22-122.39 125.59-175.91-31.511-189.63-71.766-2.514-7.3797-3.6904-10.832-3.7077-7.8964-0.0174-2.9357-1.1937 0.51669-3.7077 7.8964-13.714 40.255-67.233 197.36-189.63 71.766-64.444-66.128-34.605-132.26 82.697-152.22-67.108 11.421-142.55-7.4491-163.25-81.433-5.9562-21.282-16.111-152.36-16.111-170.07 0-88.687 77.742-60.816 125.72-24.795z" />
+        </svg>
+    );
+}
+
 export function Footer() {
     return (
         <footer className="width-full border-top bg-gray-light mx-auto max-w-[1200px] p-4 py-4">
@@ -77,6 +90,15 @@ export function Footer() {
                             className="m-1 h-9 w-9 rounded-full border-2 border-black uppercase leading-normal text-black transition duration-150 ease-in-out hover:bg-opacity-5 dark:border-white dark:text-white dark:hover:bg-black"
                         >
                             <MediumIcon />
+                        </a>
+                        <a
+                            href="https://bsky.app/profile/idmontie.bsky.social"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            type="button"
+                            className="m-1 h-9 w-9 rounded-full border-2 border-black uppercase leading-normal text-black transition duration-150 ease-in-out hover:bg-opacity-5 dark:border-white dark:text-white dark:hover:bg-black"
+                        >
+                            <BlueskyIcon />
                         </a>
                     </div>
                     <div>|</div>
